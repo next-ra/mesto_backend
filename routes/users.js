ae69972d11348e1cf0a326aeb0ce632fc5150acb
Denis Luchnikov <lasvegas3600@gmail.com>
2020-02-17 01:35:13 +0300

    added error ID route

--- routes/users.js
+++ routes/users.js
@@ -3,6 +3,20 @@ const users = require('../data/users');
 
 const router = express.Router();
 
+// получение списка пользователей
 router.get('/', (req, res) => res.json(users));
+
+// получение данных конкретного пользователя
+router.get('/:id', (req, res) => {
+  const found = users.some(user => user._id === req.params.id);
+
+  if (found) {
+    res.json(users.filter(user => user._id === req.params.id));
+  } else {
+    res
+      .status(400)
+      .json({ msg: `Нет пользователя с таким id ${req.params.id}` });
+  }
+});
 
 module.exports = router;
