fefad2e2979be3401dd78e2ff66c5d128477b9a1
Denis Luchnikov <lasvegas3600@gmail.com>
2020-05-06 05:32:16 +0300

    added users models, routes, controllers

---
+++ controllers/users.js
@@ -0,0 +1,29 @@
+const User = require('../models/user');
+
+module.exports = {
+  createUser: (req, res, next) => {
+    User.create({
+      name: req.body.name,
+      about: req.body.about,
+      avatar: req.body.avatar
+    })
+      .then(user => res.status(201).send({ data: user }))
+      .catch(err => res.send(err));
+  },
+  getUsers: (req, res, next) => {
+    User.find({})
+      .then(users =>
+        res.send({ 'количество пользователей': users.length, data: users })
+      )
+      .catch(err => res.send(err));
+  },
+  getUserById: (req, res, next) => {
+    User.findById(req.params.userId)
+      .then(user => {
+        if (!user) {
+          res.status(404).send({ message: 'Пользователь не найден' });
+        } else res.json({ data: user });
+      })
+      .catch(err => res.send(err));
+  }
+};
