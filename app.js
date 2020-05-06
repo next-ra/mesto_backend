28a8742e342d7c3c9fd7c5780f89dc620e894f76
Denis Luchnikov <lasvegas3600@gmail.com>
2020-05-06 05:11:13 +0300

    created structure of project

--- app.js
+++ app.js
@@ -1,14 +1,22 @@
 const express = require('express');
-const path = require('path');
-const PORT = require('./config');
-
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 const routes = require('./routes/routes');
 
+const { PORT = 3000 } = process.env;
 const app = express();
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(routes);
 
-app.listen(PORT, () =>
-  console.log(`Server started on port ${PORT} link: http://localhost:3000`)
-);
+mongoose.connect('mongodb://localhost:27017/mestodb', {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+  useUnifiedTopology: true
+});
+
+app.listen(PORT, () => {
+  console.log('Server is on');
+});
