4cabfe28deea56ba126b7bdc4760e64dabf1c4f8
Denis Luchnikov <lasvegas3600@gmail.com>
2020-05-06 06:19:50 +0300

    added more controllers for users

--- controllers/users.js
+++ controllers/users.js
@@ -25,5 +25,47 @@ module.exports = {
         } else res.json({ data: user });
       })
       .catch(err => res.send(err));
+  },
+  deleteUser: (req, res, next) => {
+    User.findByIdAndRemove(req.params.userId)
+      .then(user => {
+        if (!user) {
+          next();
+        } else
+          res.json({
+            message: `Пользователь с ID: ${user._id} успешно удален`
+          });
+      })
+      .catch(err => res.send(err));
+  },
+  updateUser: (req, res, next) => {
+    User.findByIdAndUpdate(
+      req.params.userId,
+      {
+        name: req.body.name,
+        about: req.body.about
+      },
+      { runValidators: true, new: true }
+    )
+
+      .then(user => {
+        if (!user) {
+          next();
+        } else res.send({ message: 'Пользователь обновлен', data: user });
+      })
+      .catch(err => res.send(err));
+  },
+  updateAvatar: (req, res, next) => {
+    User.findByIdAndUpdate(
+      req.params.userId,
+      { avatar: req.body.avatar },
+      { runValidators: true, new: true }
+    )
+      .then(user => {
+        if (!user) {
+          next();
+        } else res.send({ message: 'Аватар обновлен', data: user });
+      })
+      .catch(err => res.send(err));
   }
 };
--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,18 @@
 const router = require('express').Router();
-const { createUser, getUsers, getUserById } = require('../controllers/users');
+const {
+  createUser,
+  getUsers,
+  getUserById,
+  deleteUser,
+  updateUser,
+  updateAvatar
+} = require('../controllers/users');
 
 router.post('/', createUser);
 router.get('/', getUsers);
 router.get('/:userId', getUserById);
+router.delete('/:userId', deleteUser);
+router.patch('/:userId', updateUser);
+router.patch('/:userId/avatar', updateAvatar);
 
 module.exports = router;
