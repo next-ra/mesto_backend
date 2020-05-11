b933ad98e82f0e1ef6ace4dcf39dd00a40d450f3
Denis Luchnikov <lasvegas3600@gmail.com>
2020-05-11 16:51:56 +0300

    add limiter and helmet for more security

--- app.js
+++ app.js
@@ -2,13 +2,16 @@ require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const helmet = require('helmet');
 const routes = require('./routes/routes');
 const config = require('./config');
+const limiter = require('./middlewares/limiter');
 
 const app = express();
-
+app.use(helmet);
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(limiter);
 
 app.use(routes);
 mongoose
